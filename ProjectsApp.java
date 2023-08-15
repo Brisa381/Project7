d6a0878f896e68169b13ac93df14c5ad624b996b
Brisa381 <139829878+Brisa381@users.noreply.github.com>
2023-08-15 10:44:28 -0600

    Add files via upload

---
+++ ProjectsApp.java
@@ -0,0 +1,27 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+/**
+package projects;
+
+import projects.dao.DbConnection;
+
+/**
+ * 
+ */
+
+public class ProjectsApp {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		DbConnection.getConnection();
+		
+
+	}
+
+}
